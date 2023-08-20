080fe29dec265e337b6f53270a6b40478978649c
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-08-20 23:19:23 +0530

    js

---
+++ Untitled-1.js
@@ -0,0 +1,21 @@
+//Quokka used to show output of js directly
+x=10; y=5;
+x=x/y;
+
+//exponential
+a=2; b=2;
+a=a**b;
+
+//equal and strict equal
+p=3; q="3";
+res=p===q
+res
+
+
+x=3; y=0;
+while(x>=2)
+{
+    ++y;
+    x--;
+}
+y
