e82b7fa1ccc5cb8f1abf55064a6dde1cde41a164
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-08-20 12:25:20 +0530

    JS

---
+++ basicConcepts.js
@@ -0,0 +1,17 @@
+//This is comment
+
+//boolean datatyep
+const condition=true; 
+console.log(typeof(condition));
+
+//udefined
+var udefined;
+console.log(typeof(undefined));
+
+//String,int,float,
+
+var n=null;
+console.log(typeof(n));
+
+
+
---
+++ js.js
@@ -0,0 +1,9 @@
+function showmsg()
+        {
+            alert("Hellow fellow!!");
+        }
+function knowName()
+        {
+            var name=prompt("Enter you name:");
+            alert("welcome "+name);
+        }
